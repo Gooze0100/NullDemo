96b11adba8ca700c0f3ff046de879b27fdb90576
Giedrius <kalniusg@gmail.com>
2024-09-21 20:03:03 +0300

    null initial commit

---
+++ NullDemo/PersonModel.cs
@@ -0,0 +1,23 @@
+﻿namespace NullDemo;
+
+// you can also activate nullable option file by file if you have huge applications
+// there are 9 different options with this nullable options
+#nullable enable //annotations //warnings
+public class PersonModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsValid { get; set; }
+
+    public PersonModel()
+    {
+        Name = "";
+    }
+}
+#nullable restore //annotations //warnings
+
+public class AdderssModel
+{
+    public int StreetAddres { get; set; }
+    public string City { get; set; }
+}
